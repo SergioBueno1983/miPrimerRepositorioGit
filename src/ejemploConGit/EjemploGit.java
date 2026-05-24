681b90b327e6151aefaf5e951812b7d1151e28a8
SergioBueno1983 <SergioBueno1983@gmail.com>
2026-05-24 12:49:31 -0300

    Tercer commit, subiendo cambioos a GitHub

--- src/ejemploConGit/EjemploGit.java
+++ src/ejemploConGit/EjemploGit.java
@@ -4,8 +4,8 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		
-		//Modificando mensaje
-		System.out.println("Segundo commit en Git");
+		//Modificando mensaje y subiendolo a gitHub
+		System.out.println("Tercer commit en Git, push a GitHub");
 	
 	}
 
