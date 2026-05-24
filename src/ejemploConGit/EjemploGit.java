1cef590472aa99ece66ec540f0a73b89d343a38e
SergioBueno1983 <SergioBueno1983@gmail.com>
2026-05-24 12:02:45 -0300

    Segundo commit, modificacion de bienvenida

--- src/ejemploConGit/EjemploGit.java
+++ src/ejemploConGit/EjemploGit.java
@@ -4,7 +4,8 @@ public class EjemploGit {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Bienvenidos a Git");
+		//Modificando mensaje
+		System.out.println("Segundo commit en Git");
 	
 	}
 
